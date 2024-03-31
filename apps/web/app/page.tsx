1b9f93b46d62281277c7855371ee423613d3f9d4
Keyur-Gondaliya-403 <keyur.gondaliya@technoproject.in>
2024-04-01 03:04:59 +0530

    Feat : Flow console and view

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,37 +2,42 @@
 import React, { useState } from "react";
 import classes from "./page.module.css";
 import { useSocket } from "../context/SocketProvider";
+import Flow from "./Components/Flow";
 type Props = {};
 
 function page({}: Props) {
-  const { sendMessage, messages } = useSocket();
+  const { sendMessage, messages, status } = useSocket();
   const [message, setMessage] = useState("");
+
   return (
-    <div className={classes["container-msg"]}>
-      <div>
-        {messages.map((e, i) => (
-          <li key={e + i}>{e}</li>
-        ))}
-      </div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          sendMessage(message);
-          setMessage("");
-        }}
-      >
-        <input
-          className={classes["chat-input"]}
-          placeholder="Enter Message."
-          onChange={(e) => {
-            setMessage(e.target.value);
+    <div className={classes["main-container"]}>
+      <div className={classes["container-msg"]}>
+        <div>
+          {messages.map((e, i) => (
+            <li key={e + i}>{e}</li>
+          ))}
+        </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            sendMessage(message);
+            setMessage("");
           }}
-          value={message}
-        />
-        <button className={classes["button"]} type="submit">
-          send
-        </button>
-      </form>
+        >
+          <input
+            className={classes["chat-input"]}
+            placeholder="Enter Message."
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            value={message}
+          />
+          <button className={classes["button"]} type="submit">
+            send
+          </button>
+        </form>
+      </div>
+      <Flow status={status} />
     </div>
   );
 }
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -14,7 +14,17 @@ interface SocketProviderProps {
 interface ISocketContext {
   sendMessage: (msg: string) => any;
   messages: string[];
+  status: ServiceStatus;
 }
+export interface ServiceStatus {
+  frontend: boolean;
+  server: boolean;
+  redisSub: boolean;
+  redisPub: boolean;
+  kafka: boolean;
+  postgres: boolean;
+}
+
 const SocketContext = createContext<ISocketContext | null>(null);
 export const useSocket = () => {
   const state = useContext(SocketContext);
@@ -24,9 +34,18 @@ export const useSocket = () => {
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
   const [socket, setSocket] = useState<Socket>();
   const [messages, setMessages] = useState<string[]>([]);
+  const [status, setStatus] = useState<ServiceStatus>({
+    frontend: false,
+    server: false,
+    redisSub: false,
+    redisPub: false,
+    kafka: false,
+    postgres: false,
+  });
   const sendMessage: ISocketContext["sendMessage"] = useCallback(
     (msg) => {
-      console.log("msg", msg);
+      console.log("Sent Message to Server", msg);
+      setStatus((prev) => ({ ...prev, frontend: true }));
 
       if (socket) {
         socket.emit("event:message", { message: msg });
@@ -35,13 +54,34 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
     [socket]
   );
   const onMessageRec = useCallback((msg: string) => {
-    console.log("From server msg recived", msg);
+    console.log(
+      "Message Recived From Server to FrontEnd (While Data is adding in Redis and Kafka)",
+      msg
+    );
+    setStatus((prev) => ({ ...prev, server: true }));
+
     const { message } = JSON.parse(msg) as { message: string };
     setMessages((prev) => [...prev, message]);
   }, []);
   useEffect(() => {
     const _socket = io("http://localhost:8000");
     _socket.on("message", onMessageRec);
+    _socket.on("postgres_save", (msg: string) => {
+      console.log("Messaage Saved in Postgres.");
+      setStatus((prev) => ({ ...prev, postgres: true }));
+    });
+    _socket.on("kafka_sent", (msg: string) => {
+      console.log("Message Reach Kafka via Server");
+      setStatus((prev) => ({ ...prev, kafka: true }));
+    });
+    _socket.on("redis_sent_pub", (msg: string) => {
+      console.log("Message Sent to Redis pub via Server");
+      setStatus((prev) => ({ ...prev, redisPub: true }));
+    });
+    _socket.on("redis_sent_sub", (msg: string) => {
+      console.log("Message Sent to Server Via Redis Sub");
+      setStatus((prev) => ({ ...prev, redisSub: true }));
+    });
     setSocket(_socket);
     return () => {
       _socket.disconnect();
@@ -51,7 +91,7 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
   }, []);
 
   return (
-    <SocketContext.Provider value={{ sendMessage, messages }}>
+    <SocketContext.Provider value={{ sendMessage, messages, status }}>
       {children}
     </SocketContext.Provider>
   );
